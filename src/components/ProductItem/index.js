293b2c8a6810f3902c6841708bb8dc21164acc5e
Taras Geraschenko <tarikgera529@gmail.com>
2023-10-28 13:35:08 +0300

    nothing

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState  } from "react";
 import Header from "../Header";
 import classes from "./ProductItem.module.scss";
 import { Link } from "react-router-dom";
